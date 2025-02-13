4993a37214d5cf4d02b8dafa5099e57e975b0a68
marcelochaos <101368115+marcelochaos@users.noreply.github.com>
2025-02-13 18:17:28 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Login from "./components/login/Login.jsx"
-import "./global.scss"
-function App () {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/login/Login.jsx";
+import Reembolsos from "./components/reembolsos/Reembolsos.jsx";
+import Solicitacao from "./components/solicitacao/Solicitacao.jsx";
+import "./global.scss";
+function App() {
   return (
-   <>
-   <Login />
-   </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reembolsos" element={<Reembolsos />} />
+        <Route path="/solicitacao" element={<Solicitacao />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
